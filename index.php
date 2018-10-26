cba1b651ebf636fdd65be7f7d127990f7d396e8b
koki <k.koki0426@gmail.com>
2018-10-26 14:07:28 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+	echo 'Hello World!';
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>heroku</title>
+</head>
+<body>
+	<h1>初めてのリリース</h1>
+</body>
+</html>
